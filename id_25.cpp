c57ff65e34df5e70dafb2d2cedf33fed4bc73002
LS-01 <LS_01_phil@163.com>
2021-01-14 18:01:46 +0800

    ~

---
+++ id_25.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: id_25.cpp
+	> Author: ls
+	> Mail: 
+	> Created Time: Fri 08 Jan 2021 07:10:05 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int func(int *n1, int *n2) {
+
+}
+
+int main() {
+    int num[2][1100] = {{1, 1}, {1, 1}};
+    int a = 0, b = 1;
+    for (int i = 3; 1; i++) {
+        func(num[a], num[b])
+    }
+    return 0;
+}
